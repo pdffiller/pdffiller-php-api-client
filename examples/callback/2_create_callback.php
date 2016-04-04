aab3e10c347f323f894c1025d175fbcd3ce48aff
Serhii Kasianchuk <srg.kas@pdffiller.com>
2016-04-04 12:06:44 +0300

    KSV | development fixes

--- examples/callback/2_create_callback.php
+++ examples/callback/2_create_callback.php
@@ -2,7 +2,7 @@
 use PDFfiller\OAuth2\Client\Provider\Callback;
 $provider = require_once __DIR__ . '/../bootstrap/initWithFabric.php';
 $callback = new Callback($provider);
-//$callback->document_id = 53690143;
+$callback->document_id = 53690143;
 $callback->event_id = "fill_request.done";
 $callback->callback_url = "http://pdffiller.com/callback_destination";
 dd($callback->save());
--- examples/signature_request/1_get_signature_request_list.php
+++ examples/signature_request/1_get_signature_request_list.php
@@ -1,6 +1,6 @@
 <?php
 use PDFfiller\OAuth2\Client\Provider\SignatureRequest;
-error_reporting(32000);
+
 $provider = require_once __DIR__ . '/../bootstrap/initWithFabric.php';
 $signatureRequestEntity = new \PDFfiller\OAuth2\Client\Provider\SignatureRequest($provider);
 
