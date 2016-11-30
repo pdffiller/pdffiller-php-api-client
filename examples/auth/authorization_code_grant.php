2df6d6d852a75a509c921c7a691a5618af97b8da
osadchyi.s <sergijlys@gmail.com>
2016-11-30 11:29:33 +0200

    OSV | API-1246 | Add grant type authorization_code

--- examples/auth/authorization_code_grant.php
+++ examples/auth/authorization_code_grant.php
@@ -2,12 +2,17 @@
 
 require_once __DIR__.'/../bootstrap/init.php';
 
+$code = 'PLACE_YOUR_AUTHORIZATION_CODE_HERE';
+
 /** @var \PDFfiller\OAuth2\Client\Provider\PDFfiller $provider */
-$provider = Examples\ExampleFabric::getProvider(Examples\ExampleFabric::CLIENT_CRIDENTIALS_GRANT, [
+$provider = Examples\ExampleFabric::getProvider(Examples\ExampleFabric::AUTHORIZATION_CODE_GRANT, [
     'clientId'       => getenv('CLIENT_ID'),
     'clientSecret'   => getenv('CLIENT_SECRET'),
-    'code'           => getenv('CODE'),
-    'redirectUri'    => getenv('REDIRECT_URI')
+    'urlApiDomain'   => getenv('URL_API_DOMAIN'),
+    'urlAccessToken' => getenv('URL_ACCESS_TOKEN'),
+    'redirectUri'    => getenv('REDIRECT_URI'),
+], [
+    'code'           => $code,
 ]);
 
 dd($provider->queryApiCall('test'));
